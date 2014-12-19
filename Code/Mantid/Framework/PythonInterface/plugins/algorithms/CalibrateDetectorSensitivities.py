657f2f573444acaeca7622486a2043ac0801f609
celinedurniak <c.durniak@fz-juelich.de>
2014-12-19 10:08:20 +0100

    TOFTOF: new version of python scripts to convert tof to dE and to normalize by Vanadium.

--- Code/Mantid/Framework/PythonInterface/plugins/algorithms/CalibrateDetectorSensitivities.py
+++ Code/Mantid/Framework/PythonInterface/plugins/algorithms/CalibrateDetectorSensitivities.py
@@ -13,7 +13,7 @@ class CalibrateDetectorSensitivities(PythonAlgorithm):
     def category(self):
         """ Return category
         """
-        return "PythonAlgorithms;Utilities"
+        return "PythonAlgorithms;MLZ"
     def name(self):
         """ Return summary
         """
@@ -28,7 +28,38 @@ class CalibrateDetectorSensitivities(PythonAlgorithm):
         self.declareProperty(WorkspaceProperty("InputWorkspace","",direction=Direction.Input), "Input Vanadium workspace")
         self.declareProperty(WorkspaceProperty("OutputWorkspace","", direction=Direction.Output), "Name the workspace that will contain the result")
         return
-
+    
+    def _FitGaussiang(self,ws):
+        nb_hist = ws.getNumberHistograms()
+        TableFit=np.zeros((nb_hist, 2))#4))
+        for i in range(nb_hist):
+            x_values = ws.readX(i)
+            y_values = ws.readY(i)
+            if np.max(y_values) !=0:
+                imax=np.argmax(y_values)
+                ymax=y_values[imax]
+                tryCentre = x_values[imax]
+                height = ymax# start guess on peak height
+                inx = np.argwhere(y_values>0.5*height)
+                sigma = x_values[inx[len(inx)-1][0]]-x_values[inx[0][0]] # start guess on peak width
+                fitAlg = self.createChildAlgorithm('Fit')
+                fitAlg.setProperty('Function','name=Gaussian, Height='+str(height)+', PeakCentre='+str(tryCentre)+', Sigma='+str(sigma))
+                fitAlg.setProperty('InputWorkspace', ws)
+                fitAlg.setProperty('WorkspaceIndex', i)
+                fitAlg.setProperty('StartX', tryCentre-sigma)
+                fitAlg.setProperty('EndX', tryCentre+sigma)
+                fitAlg.setProperty('MaxIterations',10)
+                fitAlg.setProperty('CreateOutput',True)
+                fitAlg.execute()
+                paramTable = fitAlg.getProperty('OutputParameters').value
+                TableFit[i,0]=paramTable.column(1)[1]
+                TableFit[i,1]=paramTable.column(1)[2]
+                #TableFit[i,2]=paramTable.column(2)[1]
+                #TableFit[i,3]=paramTable.column(2)[2]               
+            else:
+                TableFit[i,:]=0.
+        return TableFit
+    
     def PyExec(self):
         """ Main execution body
         """
@@ -43,7 +74,6 @@ class CalibrateDetectorSensitivities(PythonAlgorithm):
 
         nb_block=self._wksp.blocksize()
         nb_hist = self._wksp.getNumberHistograms()
-        self._owksp = self._wksp+0.
 
         # See Sears and Shelley Acta Cryst. A 47, 441 (1991)
         DWF=np.zeros(nb_hist) #Debye Waller factor corection. One value per spectrum or detector
@@ -55,61 +85,56 @@ class CalibrateDetectorSensitivities(PythonAlgorithm):
                         (det.getPos().Z())**2)
             thetasort[i]= np.sign(det.getPos().X())*np.arccos(det.getPos().Z()/r)/2.
             
-        if not str(run.getLogData('temperature').value):
-            Tempk=293. # in K. assume room temperature if not defined in input file
-            
+        if not 'temperature' in run.keys() or not str(run.getLogData('temperature').value):
+            Tempk=293. # in K. assumes room temperature if not defined in input file
+            logger.warning("No temperature was given. T= %f K is assumed for the Debye Waller factor." % Tempk)
+            print "No temperature was given. T= ",Tempk," K is assumed for the Debye Waller factor."
         else:
             Tempk=float(run.getLogData('temperature').value)
-        wlength = float(run.getLogData('wavelength').value) #in angstroem
-        Tm=389. # in K. from Sears paper
-        MVana=50.942/1000./sp.constants.N_A # Vanadium atomic mass
+    
+        wlength = float(run.getLogData('wavelength').value) #in AA
+        Tm = 389. # in K. from Sears paper
+        MVana = 50.942/1000./sp.constants.N_A # Vanadium atomic mass
         if (Tempk < 1.e-3*Tm):
             integral = 0.5
         else:
             y=Tempk/Tm
-        def integrand(x,y):
-            return x/sp.tanh(x/2./y)
-        integral = quad(integrand,0,1,args=(y))[0]
+            def integrand(x,y):
+                return x/sp.tanh(x/2./y)
+            integral = quad(integrand,0,1,args=(y))[0]
+            
         u2=3.*sp.constants.hbar**2/(2.*MVana*sp.constants.k *Tm)*integral*1.e20
         DWF=np.exp(-u2*(4.*sp.pi/wlength*sp.sin(thetasort))**2)
 
         array_count_vanadium=np.zeros(nb_block)
         array_error_vanadium=np.zeros(nb_block)
+        arrayx=self._wksp.readX(0)
+        
+        self._owksp = self._wksp+0.
+        
+        Table=self._FitGaussiang(self._wksp)
+        
         for i in range(nb_hist):
             #crop around max EPP + fit to get fwhm
             array=self._wksp.readY(i)
-            arrayx=self._wksp.readX(i)
-            arrayerr=self._wksp.readE(i)
             if np.max(array) !=0:
-                #First guess for fitting parameters
-                tryCentre = arrayx[np.argmax(array)]
-                height = np.max(array)# start guess on peak height
-                inx = np.argwhere(array>0.5*height)
-                sigma = (arrayx[inx[len(inx)-1][0]]-arrayx[inx[0][0]])/2.*np.sqrt(2.*np.log(2.)) # start guess on peak width
-                #Fitting windows
-                startx = tryCentre-10.*sigma
-                endx = tryCentre+10.*sigma
-                myFunc = 'name=Gaussian, Height='+str(height)+', PeakCentre='+str(tryCentre)+', Sigma='+str(sigma) 
-                #Run fitting
-                fitStatus, chiSq, covarianceTable, paramTable, fitWorkspace = Fit(InputWorkspace=self._wksp, \
-                WorkspaceIndex=i, StartX = startx, EndX=endx, Output='fitVanadium', Function=myFunc, MaxIterations=100)
-                #Definition of fwhm to crop vanadium counts and sum
-                fwhm= 2.*np.sqrt(2.*np.log(2.))*paramTable.column(1)[2]
-                peakCentre = paramTable.column(1)[1]
-                #print fwhm, peakCentre, height
+                arrayerr=self._wksp.readE(i)
+                fwhm= 2.*np.sqrt(2.*np.log(2.))*Table[i,1]
+                peakCentre = Table[i,0]
                 idxmin = (np.abs(arrayx-peakCentre+3.*fwhm)).argmin()
                 idxmax = (np.abs(arrayx-peakCentre-3.*fwhm)).argmin()
-                array_count_vanadium[:]=DWF[i]*sum(array[j] for j in range(idxmin,idxmax+1))
-                array_error_vanadium[:]=np.sqrt(DWF[i])*sum(arrayerr[j] for j in range(idxmin,idxmax+1))
+                
+                elt_county_vanadium=DWF[i]*sum(array[j] for j in range(idxmin,idxmax+1))
+                elt_counte_vanadium=DWF[i]*sum(arrayerr[j] for j in range(idxmin,idxmax+1))
             else:
-                array_count_vanadium[:]=0.
-                array_error_vanadium[:]=0.
+                elt_county_vanadium=0.
+                elt_counte_vanadium=0.
+            array_count_vanadium[:]=elt_county_vanadium
+            array_error_vanadium[:]=elt_counte_vanadium
             #Fill output workspace
-            self._owksp.setX(i,arrayx)
             self._owksp.setY(i,array_count_vanadium)
             self._owksp.setE(i,array_error_vanadium)
            
-
         self.setProperty("OutputWorkspace",self._owksp)      
     
 #########################################################################################
--- Code/Mantid/Framework/PythonInterface/plugins/algorithms/Tof2dE.py
+++ Code/Mantid/Framework/PythonInterface/plugins/algorithms/Tof2dE.py
@@ -13,7 +13,7 @@ class Tof2dE(PythonAlgorithm):
     def category(self):
         """ Return category
         """
-        return "PythonAlgorithms;Utilities"
+        return "PythonAlgorithms;MLZ"
     def name(self):
         """ Return summary
         """
@@ -25,57 +25,80 @@ class Tof2dE(PythonAlgorithm):
     def PyInit(self):
         """ Declare properties
         """
-
         self.declareProperty(WorkspaceProperty("InputWorkspace","",direction=Direction.Input), "Input Sample workspace")
         #Optional but required if choice of tof_elastic from Vanadium ws or file
-        self.declareProperty(MatrixWorkspaceProperty("Workspace","",direction=Direction.Input,optional = PropertyMode.Optional), "Input Vanadium workspace (optional)")
+        self.declareProperty(MatrixWorkspaceProperty("Workspace","", direction=Direction.Input,optional = PropertyMode.Optional), "Input Vanadium workspace (optional)")
         self.declareProperty(WorkspaceProperty("OutputWorkspace","", direction=Direction.Output), "Name the workspace that will contain the result")
         choice_tof = ["Geometry", "FitVanadium","FitSample"]
         self.declareProperty("ChoiceElasticTof", "Geometry",StringListValidator(choice_tof))
 
         return
 
-    def _FitGaussian(self,ws):
-        x_values = ws.readX(1)
-        y_values = np.sum(ws.extractY(), axis=0)
-        imax=np.argmax(y_values)
-        ymax=y_values[imax]
-        tryCentre = x_values[imax]
-        height = ymax# start guess on peak height
-        inx = np.argwhere(y_values>0.5*height)
-        sigma = x_values[inx[len(inx)-1][0]]-x_values[inx[0][0]] # start guess on peak width
-    #    ## Function to fit
-        myFunc = 'name=Gaussian, Height='+str(height)+', PeakCentre='+str(tryCentre)+', Sigma='+str(sigma)
-    #    ##Fitting window
-        startx = tryCentre-10.*sigma
-        endx = tryCentre+10.*sigma
-        ##Run fitting of vector not workspace
-        dataWS = CreateWorkspace(DataX=x_values, DataY=y_values)
-        fitStatus, chiSq, covarianceTable, paramTable, fitWorkspace = Fit(InputWorkspace=dataWS, \
-        StartX = startx, EndX=endx, Output='fitGaussian', Function=myFunc)
-        return paramTable.column(1)[1]
+
+    def _FitGaussiang(self,ws):
+        nb_hist = ws.getNumberHistograms()
+        TableFit=np.zeros(nb_hist)
+        for i in range(nb_hist):
+            x_values = ws.readX(i)
+            y_values = ws.readY(i)
+            if np.max(y_values) !=0:
+                imax=np.argmax(y_values)
+                ymax=y_values[imax]
+                tryCentre = x_values[imax]
+                height = ymax# start guess on peak height
+                inx = np.argwhere(y_values>0.5*height)
+                sigma = x_values[inx[len(inx)-1][0]]-x_values[inx[0][0]] # start guess on peak width
+                fitAlg = self.createChildAlgorithm('Fit')
+                print i, 'initial guess: height ',height,', centre: ',tryCentre,'sigma ', sigma,'startX',tryCentre-5.*sigma,'endX',tryCentre+5.*sigma  
+                fitAlg.setProperty('Function','name=Gaussian, Height='+str(height)+', PeakCentre='+str(tryCentre)+', Sigma='+str(sigma))
+                fitAlg.setProperty('InputWorkspace', ws) 
+                fitAlg.setProperty('WorkspaceIndex', i)
+                fitAlg.setProperty('StartX', tryCentre-10.*sigma)
+                fitAlg.setProperty('EndX', tryCentre+10.*sigma)
+                fitAlg.setProperty('Output','fitGaussianTof2dE')
+                fitAlg.setProperty('CreateOutput',True)
+                fitAlg.execute()
+                paramTable = fitAlg.getProperty('OutputParameters').value
+              
+                TableFit[i]=paramTable.column(1)[1]
+                #print i, paramTable.column(1)[0],paramTable.column(2)[0],paramTable.column(1)[1],paramTable.column(2)[1],paramTable.column(1)[2],paramTable.column(2)[2]
+            else:
+                TableFit[i]=0.
+        return TableFit
+
+    def _FitLorentziang(self,ws):
+        nb_hist = ws.getNumberHistograms()
+        TableFit=np.zeros(nb_hist)
+        for i in range(nb_hist):
+            x_values = ws.readX(i)
+            y_values = ws.readY(i)
+            if np.max(y_values) !=0:
+                imax=np.argmax(y_values)
+                ymax=y_values[imax]
+                tryCentre = x_values[imax]
+                height = ymax# start guess on peak height
+                inx = np.argwhere(y_values>0.5*height)
+                sigma = x_values[inx[len(inx)-1][0]]-x_values[inx[0][0]] # start guess on peak width
+                print "First guess for index", i, ": height",height ,"centre:" ,tryCentre ,"sigma",sigma,'startX',tryCentre-5.*sigma,'endX',tryCentre+5.*sigma
+                fitAlg = self.createChildAlgorithm('Fit')  
+                fitAlg.setProperty('Function','name=LinearBackground, A0=0; name=Lorentzian, Amplitude='+str(height)+\
+        ', PeakCentre='+str(tryCentre)+', FWHM='+str(sigma))
+                fitAlg.setProperty('InputWorkspace', ws) 
+                fitAlg.setProperty('WorkspaceIndex', i)
+                fitAlg.setProperty('StartX', tryCentre-5.*sigma)
+                fitAlg.setProperty('EndX', tryCentre+5.*sigma)
+                
+                fitAlg.setProperty('CreateOutput',True)
+                fitAlg.setProperty('Output','fitLorentzian')
+                fitAlg.setProperty('constraints','f1.PeakCentre>0,f1.FWHM>0')
+                fitAlg.execute()
+                paramTable = fitAlg.getProperty('OutputParameters').value
+                TableFit[i]=paramTable.column(1)[3]
+                print i, paramTable.column(1)[0],paramTable.column(1)[1],paramTable.column(1)[2],paramTable.column(1)[3],paramTable.column(1)[4],
+            else:
+                TableFit[i]=0.
+        return TableFit
     
-    def _FitLorentzian(self,ws):
-        x_values = ws.readX(1)
-        y_values = np.sum(ws.extractY(), axis=0)
-        imax=np.argmax(y_values)
-        ymax=y_values[imax]
-        tryCentre = x_values[imax]
-        height = ymax# start guess on peak height
-        inx = np.argwhere(y_values>0.5*height)
-        sigma = x_values[inx[len(inx)-1][0]]-x_values[inx[0][0]] # start guess on peak width
-    #    ## Function to fit
-        myFunc = 'name=LinearBackground, A0=0; name=Lorentzian, Amplitude='+str(height)+\
-        ', PeakCentre='+str(tryCentre)+', FWHM='+str(sigma)
-    #    ##Fitting window
-        startx = tryCentre-10.*sigma
-        endx = tryCentre+10.*sigma
-        ##Run fitting of vector not workspace
-        dataWS = CreateWorkspace(DataX=x_values, DataY=y_values)
-        fitStatus, chiSq, covarianceTable, paramTable, fitWorkspace = Fit(InputWorkspace=dataWS,\
-        StartX = startx, EndX=endx, Output='fitLorentzian', Function=myFunc,\
-        constraints='f1.PeakCentre>0,f1.FWHM>0')
-        return paramTable.column(1)[1]
 
     def PyExec(self):
         """ Main execution body
@@ -88,41 +111,44 @@ class Tof2dE(PythonAlgorithm):
             raise ValueError('Wrong instrument')
 
         run=self._wksp.getRun()
-
         a=self.getProperty("ChoiceElasticTof").value
- 
+        
+        nb_block = self._wksp.blocksize()
+        nb_hist = self._wksp.getNumberHistograms()
+        
         if a=='Geometry':
             # tof_elastic from header of raw datafile
-            print a
-            tof_elastic=float(run.getLogData('EPP').value)*float(run.getLogData('channel_width').value) #in microsecs
+            tof_elastic=np.zeros(nb_hist)
+            tof_elastic[:] = float(run.getLogData('EPP').value)*float(run.getLogData('channel_width').value) #in microsecs
+
         if a=='FitSample':
-            print a
-            tof_elastic = self._FitLorentzian(self._wksp)
+            tof_elastic = self._FitLorentziang(self._wksp)
         if a=='FitVanadium':
             self._wkspvana = self.getProperty("Workspace").value
-            print a
-            tof_elastic = self._FitGaussian(self._wkspvana)
+            tof_elastic = self._FitGaussiang(self._wkspvana)
         
-        print float(run.getLogData('EPP').value)*float(run.getLogData('channel_width').value),tof_elastic
         sample = self._wksp.getInstrument().getSample() 
         samplePos = sample.getPos()
         sourcePos = self._wksp.getInstrument().getSource().getPos()
         Lsd = self._wksp.getInstrument().getDetector(1).getDistance(sample)
 
-        nb_block = self._wksp.blocksize()
-        nb_hist = self._wksp.getNumberHistograms()
-
         self._owksp = self._wksp+0.
 
-        arrayx=np.zeros(nb_block+1)
-        print tof_elastic
-        for i in range(nb_block+1):
-            arrayx[i]=0.5*sp.constants.m_n*Lsd**2*(1/self._wksp.readX(1)[i]**2-1/tof_elastic**2)*10.**15/sp.constants.eV
-            print arrayx[i]
-     
-        for j in range(nb_hist):
-            self._owksp.setX(j,arrayx)
+        arrayx=np.zeros(nb_block+1) # will be filled for each histogram
+
+        dx = sp.array([self._wksp.readX(1)[i]-self._wksp.readX(1)[i-1] for i in range(1,len(arrayx))])
+        arrayynorm=sp.array([(self._wksp.readX(1)[i]/1.e6)**3*sp.constants.eV*1.e3/(sp.constants.m_n*Lsd**2*dx[i]) for i in range(nb_block)])
         
+        for j in range(nb_hist):
+            if tof_elastic[j] != 0.:
+                for i in range(nb_block+1):
+                    arrayx[i]=0.5*sp.constants.m_n*Lsd**2*(1./self._wksp.readX(j)[i]**2-1./tof_elastic[j]**2)*10.**15/sp.constants.eV
+            self._owksp.setX(j,arrayx)
+            newy=arrayynorm*self._owksp.readY(j)
+            newe=arrayynorm*self._owksp.readE(j)
+            self._owksp.setY(j,newy)
+            self._owksp.setE(j,newe)
+              
         self._owksp.getAxis(0).setUnit('DeltaE')
            
         self.setProperty("OutputWorkspace",self._owksp)      
