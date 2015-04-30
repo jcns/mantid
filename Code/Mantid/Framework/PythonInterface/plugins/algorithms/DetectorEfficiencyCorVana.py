70ca241cefbc06a81bf889624127d29bec13ce50
Marina Ganeva <m.ganeva@fz-juelich.de>
2015-04-30 16:52:29 +0200

    DNS Vanadium correction algorithm. Corrections.

--- Code/Mantid/Framework/PythonInterface/plugins/algorithms/DetectorEfficiencyCorVana.py
+++ Code/Mantid/Framework/PythonInterface/plugins/algorithms/DetectorEfficiencyCorVana.py
@@ -2,10 +2,11 @@ from mantid.kernel import *
 from mantid.api import *
 import mantid.simpleapi as api
 import numpy as np
-
 import os, sys
 
+
 NORMALIZATIONS = ['duration', 'mon_sum']
+
 
 class DetectorEfficiencyCorVana(PythonAlgorithm):
     """
@@ -31,7 +32,7 @@ class DetectorEfficiencyCorVana(PythonAlgorithm):
                 on a given data workspace using the Vanadium data."
 
     def PyInit(self):
-        self.declareProperty(WorkspaceProperty("InputWorkspace", \
+        self.declareProperty(WorkspaceProperty("InputWorkspace",
                 "", direction=Direction.Input), \
                 doc="A workspace with raw experimental data.")
         self.declareProperty(WorkspaceProperty("OutputWorkspace", \
@@ -51,6 +52,8 @@ class DetectorEfficiencyCorVana(PythonAlgorithm):
 
 
     def PyExec(self):
+        # [TODO:] check whether input workspace is one of the "created"
+        # to avoid the infinite recursion
         # Input
         dataws = mtd[self.getPropertyValue("InputWorkspace")]
         outws = self.getPropertyValue("OutputWorkspace")
@@ -65,33 +68,39 @@ class DetectorEfficiencyCorVana(PythonAlgorithm):
             raise ValueError("The dimensions of Vanadium workspace are not valid.")
         if (bkgws.getNumDims() != nd or bkgws.getNumberHistograms() != nh):
             raise ValueError("The dimensions of Background workspace are not valid.")
-        run = dataws.getRun()
+        bkg_run = bkgws.getRun()
+        vana_run = vanaws.getRun()
         # [TODO:] check slits, produce warning if different
+        # [TODO:] check deterota, complain if it is out of tolerance range
         # 1. normalize Vanadium and Background
         try:
-            nvalue = run.getProperty(norm).value
+            bkg_nvalue = bkg_run.getProperty(norm).value
+            vana_nvalue = vana_run.getProperty(norm).value
         except:
             raise ValueError("The normalization parameter %s is not valid" % norm)
         else:
             # numpy.full is not supported by 'mantid' numpy!
             # dataY = np.full((nd-1, nh), nvalue)
             dataY = np.empty((nd-1, nh))
-            dataY.fill(nvalue)
+            dataY.fill(bkg_nvalue)
             dataX = np.zeros(nh)
             dataE = np.zeros((nd-1, nh)) # the monitor error is neglected
-            normws = api.CreateWorkspace(DataX=dataX, \
+            bkg_normws = api.CreateWorkspace(DataX=dataX, \
                 DataY=dataY, DataE=dataE, NSpec=nh, UnitX="Wavelength")
-            vana_norm = api.Divide(vanaws, normws)
-            bkg_norm = api.Divide(bkgws, normws)
+            dataY.fill(vana_nvalue)
+            vana_normws = api.CreateWorkspace(DataX=dataX, \
+                DataY=dataY, DataE=dataE, NSpec=nh, UnitX="Wavelength")
+            vana_norm = api.Divide(vanaws, vana_normws)
+            bkg_norm = api.Divide(bkgws, bkg_normws)
             # 2. substract background from Vanadium
             vana_bg = vana_norm - bkg_norm
-            # [TODO:] check negative values, complain lauid
+            # [TODO:] check negative values, complain laud
             # 3. calculate correction coefficients
+            # [TODO:] Make IncludeMonitors to external parameter
             vana_mean_ws = api.SumSpectra(vana_bg, IncludeMonitors=True) / nh
             coef_ws = api.Divide(vana_bg, vana_mean_ws)
             # 4. correct raw data (not normalized!)
             __corrected_data__ = api.Divide(dataws, coef_ws)
-
 
             self.setProperty("OutputWorkspace", __corrected_data__)
             self.log().debug('DetectorEfficiencyCorVana: OK')
