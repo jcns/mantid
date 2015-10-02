99d974463b7ba95071d5ad25d7175815d62233bd
Marina Ganeva <m.ganeva@fz-juelich.de>
2015-10-02 15:48:53 +0200

    Refactoring of TOFTOFMergeRuns (formal TOFTOFCleanSampleLogs), its unittests and documentation.

--- Code/Mantid/Framework/PythonInterface/test/python/plugins/algorithms/TOFTOFMergeRunsTest.py
+++ Code/Mantid/Framework/PythonInterface/test/python/plugins/algorithms/TOFTOFMergeRunsTest.py
@@ -1,11 +1,11 @@
 import unittest
-from mantid.simpleapi import Load, CloneWorkspace, DeleteWorkspace, TOFTOFCleanSampleLogs, AddSampleLogMultiple, \
+from mantid.simpleapi import Load, DeleteWorkspace, AddSampleLogMultiple, \
     DeleteLog
 from testhelpers import run_algorithm
 from mantid.api import AnalysisDataService
 
 
-class TOFTOFCleanSampleLogsTest(unittest.TestCase):
+class TOFTOFMergeRunsTest(unittest.TestCase):
 
     def setUp(self):
         input_ws = Load(Filename="TOFTOFTestdata.nxs")
@@ -24,7 +24,7 @@ class TOFTOFCleanSampleLogsTest(unittest.TestCase):
         OutputWorkspaceName = "output_ws"
         Inputws = "%s, %s" % (self._input_ws_base.name(), self._input_good.name())
 
-        alg_test = run_algorithm("TOFTOFCleanSampleLogs",
+        alg_test = run_algorithm("TOFTOFMergeRuns",
                                  InputWorkspaces=Inputws,
                                  OutputWorkspace=OutputWorkspaceName)
         self.assertTrue(alg_test.isExecuted())
@@ -39,25 +39,26 @@ class TOFTOFCleanSampleLogsTest(unittest.TestCase):
         self.assertEqual(run_out.getLogData('channel_width').value, run_in.getLogData('channel_width').value)
         self.assertEqual(run_out.getLogData('Ei').value, run_in.getLogData('Ei').value)
         self.assertEqual(run_out.getLogData('EPP').value, run_in.getLogData('EPP').value)
-        self.assertEqual(run_out.getLogData('proposal_number').value, int(run_in.getLogData('proposal_number').value))
+        self.assertEqual(run_out.getLogData('proposal_number').value, run_in.getLogData('proposal_number').value)
         self.assertEqual(run_out.getLogData('proposal_title').value, run_in.getLogData('proposal_title').value)
         self.assertEqual(run_out.getLogData('mode').value, run_in.getLogData('mode').value)
         self.assertEqual(run_out.getLogData('experiment_team').value, run_in.getLogData('experiment_team').value)
 
         run_in_good = self._input_good.getRun()
-        self.assertEqual(run_out.getLogData('run_number').value, str([run_in.getLogData('run_number').value,
-                                                                  run_in_good.getLogData('run_number').value]))
+        self.assertEqual(run_out.getLogData('run_number').value,
+                         str([run_in.getLogData('run_number').value, run_in_good.getLogData('run_number').value]))
 
         self.assertEqual(run_out.getLogData('temperature').value, float(run_in.getLogData('temperature').value))
-        self.assertEqual(run_out.getLogData('duration').value, float(run_in.getLogData('duration').value))
+        self.assertEqual(run_out.getLogData('duration').value,
+                         float(run_in.getLogData('duration').value) + float(run_in_good.getLogData('duration').value))
         self.assertEqual(run_out.getLogData('run_start').value, run_in.getLogData('run_start').value)
         self.assertEqual(run_out.getLogData('run_end').value, run_in.getLogData('run_end').value)
         self.assertEqual(run_out.getLogData('full_channels').value, run_in.getLogData('full_channels').value)
         self.assertEqual(run_out.getLogData('monitor_counts').value, 2*int(run_in.getLogData('monitor_counts').value))
-        ## Dimension output workspace
+        # Dimension output workspace
         self.assertEqual(wsoutput.getNumberHistograms(), self._input_ws_base.getNumberHistograms())
         self.assertEqual(wsoutput.blocksize(), self._input_ws_base.blocksize())
-        ## check instrument
+        # check instrument
         self.assertEqual(wsoutput.getInstrument().getName(), "TOFTOF")
 
         AnalysisDataService.remove("output_ws")
@@ -69,14 +70,14 @@ class TOFTOFCleanSampleLogsTest(unittest.TestCase):
         OutputWorkspaceName = "output_ws"
         Inputws_badvalue = "%s, %s" % (self._input_ws_base.name(), self._input_bad_value.name())
         self.assertRaises(RuntimeError,
-                          run_algorithm, 'TOFTOFCleanSampleLogs',
+                          run_algorithm, 'TOFTOFMergeRuns',
                           InputWorkspaces=Inputws_badvalue,
                           OutputWorkspace=OutputWorkspaceName,
                           rethrow=True)
 
         Inputws_badentry = "%s, %s" % (self._input_ws_base.name(), self._input_bad_entry.name())
         self.assertRaises(RuntimeError,
-                          run_algorithm, 'TOFTOFCleanSampleLogs',
+                          run_algorithm, 'TOFTOFMergeRuns',
                           InputWorkspaces=Inputws_badentry,
                           OutputWorkspace=OutputWorkspaceName,
                           rethrow=True)
@@ -88,5 +89,5 @@ class TOFTOFCleanSampleLogsTest(unittest.TestCase):
         if self._input_ws_base is not None:
             DeleteWorkspace(self._input_ws_base)
 
-if __name__=="__main__":
+if __name__ == "__main__":
     unittest.main()
